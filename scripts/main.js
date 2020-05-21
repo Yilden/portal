2ef33a50283f3705a16847b2d1ba42475af1cc57
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-05-22 00:17:17 +0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+//blocks
+
+  //environment
+  require("blocks/environment/blacktile");
+  require("blocks/environment/whitetile");
+  require("blocks/environment/squaredtile");
+  
+  //production
+  //require("blocks/production/hardlight");
+  
+  //producer
+  //require("blocks/producer/clauncher");
+  
+  //power
+  require("blocks/power/fbutton");
+  require("blocks/power/pcircle");
+  
+//liquids
+  require("liquids/propulsion");
+  require("liquids/repulsion");
+  require("liquids/conversion");
+  //require("liquids/pure");
+  
+//units
+  //require("units/sentry");
+  require("units/storagecube");
+  require("units/compcube");
