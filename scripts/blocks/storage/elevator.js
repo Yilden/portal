3f0829f9d1de26ac84b871c029f70d6e1344338e
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-05-26 12:02:48 +0700

    elevator.js onUnitRespawn()

--- scripts/blocks/storage/elevator.js
+++ scripts/blocks/storage/elevator.js
@@ -9,7 +9,21 @@ const elv = extendContent(CoreBlock, "elevator", {
     return [
 		 Core.atlas.find(this.name)	
 		]
+  },
+  setStats(){
+    //h
+  },
+  onUnitRespawn(tile, player){
+    if(player == null || tile.entity == null) return;
+        entity = tile.ent();
+        Effects.effect(Fx.spawn, entity);
+        entity.progress = 0;
+        entity.spawnPlayer = player;
+        entity.spawnPlayer.onRespawn(tile);
+        entity.spawnPlayer.applyImpulse(0, 8);
+        entity.spawnPlayer = null;
   }
+  
 		
 });
 
