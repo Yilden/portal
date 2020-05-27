a76336ac67c66823d00a5da8a75da55acd96eb8b
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-05-27 15:05:56 +0700

    hhhhh

--- scripts/blocks/walls/portaldoor.js
+++ scripts/blocks/walls/portaldoor.js
@@ -1,1 +1,23 @@
-const door = extendContent(Door, "portal-door", {});
+const door = extendContent(Door, "portal-door", {
+ load(){
+   this.super$load();
+   
+   this.region = Core.atlas.find(this.name);
+   this.openRegion = Core.atlas.find(this.name + "-open");
+  }, 
+  draw(tile){
+    entity = tile.ent();
+    
+    if(entity.open){
+      Draw.rect(this.openRegion, tile.drawx(), tile.drawy());
+    }
+    else {
+      Draw.rect(this.region, tile.drawx(), tile.drawy());
+    }
+  },
+  update(tile){
+    if(tile.entity.power.status > 0){
+      Call.onDoorToggle()
+    }
+  }
+});
