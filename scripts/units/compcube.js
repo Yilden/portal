7c8bdaccf7bab3e897b152c562b277c0cd1ebedb
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-05-28 18:31:51 +0700

    try with this.type.region

--- scripts/units/compcube.js
+++ scripts/units/compcube.js
@@ -29,7 +29,7 @@ wcc.create(prov(() => new JavaAdapter(GroundUnit, {
         Draw.color(Color.white);
     }
     
-    Draw.rect(this.region, this.x, this.y, this.rotation - 90);
+    Draw.rect(this.type.region, this.x, this.y, this.rotation - 90);
     Draw.mixcol();
   },
   
