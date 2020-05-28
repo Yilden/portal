0996852f36bed2a9716dbbfa203ca271cce0b284
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-05-28 12:15:08 +0700

    Remove onUnitRespawn

--- scripts/blocks/storage/elevator.js
+++ scripts/blocks/storage/elevator.js
@@ -10,21 +10,13 @@ const elv = extendContent(CoreBlock, "elevator", {
     this.region = Core.atlas.find(this.name);
     this.topRegion = Core.atlas.find(this.name + "-top");
   },
+	
   generateIcons(){
     return [
       Core.atlas.find(this.name)	
     ]
   },
-  onUnitRespawn(tile, player){
-    if(player == null || tile.entity == null) return;
-       entity = tile.ent();
-       Effects.effect(effectA, entity);
-       entity.progress = 0;
-       entity.spawnPlayer = player;
-       entity.spawnPlayer.onRespawn(tile);
-       entity.spawnPlayer.applyImpulse(0, 8);
-       entity.spawnPlayer = null;
-  },
+  
   draw(tile){
     entity = tile.ent();
     
