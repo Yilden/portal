21033735a4b0960eac3dee770b6ea579ada21d1e
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-05-28 17:05:52 +0700

    sk7725's armorstand.js update() copypaste

--- scripts/units/storagecube.js
+++ scripts/units/storagecube.js
@@ -43,5 +43,52 @@ wsc.create(prov(() => new JavaAdapter(GroundUnit, {
   
   countsAsEnemy(){
     return false;
+  },
+   
+  updateTargeting(){
+    if(this.target != null){
+      this.target = null;
+    }
+  },
+    
+  update(){
+    //this.super$update();
+    //BaseUnit
+    if(this.isDead()){
+    //dead enemies should get immediately removed
+      this.remove();
+      return;
+    }
+    this.hitTime -= Time.delta();
+    if(Vars.net.client()){
+      this.interpolate();
+      this.status.update(this);
+      return;
+    }
+    if(!this.isFlying() && (Vars.world.tileWorld(this.x, this.y) != null && !(Vars.world.tileWorld(this.x,this.y).block() instanceof BuildBlock) && Vars.world.tileWorld(this.x, this.y).solid())){
+      //when it is stuck in a WALL
+      //this.kill();
+    }
+    this.avoidOthers();
+    if(this.spawner != this.noSpawner && (Vars.world.tile(this.spawner) == null || !(Vars.world.tile(this.spawner).entity instanceof UnitFactoryEntity))){
+      //when its factory is in a COFFIN
+      this.kill();
+    }
+    this.updateTargeting();
+    //this.state.update(); //braindead
+    this.updateVelocityStatus();
+    //if(this.target != null) this.behavior();
+    if(!this.isFlying()){
+      this.clampPosition();
+    }
+
+    //GroundUnit
+    this.stuckTime = !this.vec.set(this.x, this.y).sub(this.lastPosition()).isZero(0.0001) ? 0 : this.stuckTime + Time.delta();
+    if(!this.velocity().isZero()){
+      this.baseRotation = Mathf.slerpDelta(this.baseRotation, this.velocity().angle(), 0.05);
+    }
+    if(this.stuckTime < 1.0){
+      this.walkTime += Time.delta();
+    }
   }
 })));
