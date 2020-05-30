f994b9499b3c1823f3c96c099235a0068cd2d031
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-05-30 14:42:12 +0700

    Create pbutton.js

---
+++ scripts/blocks/power/pbutton.js
@@ -0,0 +1,25 @@
+const pressTick = 6;
+const timerId = 0;
+const button = extendContent(Block, "pedestal", {
+  placed(tile) {
+    this.super$placed(tile);
+    tile.ent().timer.reset(timerId, pressTick + 1);
+  },
+  draw(tile) {
+    Draw.rect(Core.atlas.find(this.name + ((tile.ent().timer.check(timerId, pressTick)) ? "":"-on")), tile.drawx(), tile.drawy());
+  },
+  tapped(tile, player){
+    tile.ent().timer.reset(timerId,0);
+    Sounds.click.at(tile.worldx(),tile.worldy());
+  },
+/*
+  update(tile){
+    if(tile.ent().timer.getTime(timerId) == pressTick) Sounds.click.at(tile.worldx(),tile.worldy(), 0.8);
+  }
+*/
+  getPowerProduction(tile){
+  return (tile.ent().timer.check(timerId, pressTick)) ? 0: 6;
+  }
+});
+
+//Credits: sk7725, check out sk7725/Commandblocks.
