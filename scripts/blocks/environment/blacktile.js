ea9274f62c2af03553926f5e1e85ead838a01044
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-05-31 20:19:50 +0700

    test

--- scripts/blocks/environment/blacktile.js
+++ scripts/blocks/environment/blacktile.js
@@ -3,22 +3,13 @@ const inferior = extendContent(Floor, "black-tile", {
     this.region = Core.atlas.find(this.name);
   },
   
-  generateIcons: function(){
+  generateIcons(){
     return [
       Core.atlas.find(this.name)
     ];
   },
   
-  draw: function(tile){
-    Mathf.random.setSeed(tile.pos());
-
-      Draw.rect(variantRegions[Mathf.randomSeed(tile.pos(), 0, Math.max(0, variantRegions.length - 1))], tile.worldx(), tile.worldy());
-
-      //drawEdges(tile);
-
-      Floor floor = tile.overlay();
-      if(floor != Blocks.air && floor != this){ //ore should never have itself on top, but it's possible, so prevent a crash in that case
-        floor.draw(tile);
-        }
+  draw(tile){
+    Draw.rect(this.region, tile.worldx(), tile.worldy());
   }
 });
