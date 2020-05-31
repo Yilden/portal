0a5369c367ca311647dbe82f8bed8be09764538a
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-05-31 17:43:29 +0700

    Hmm, yes

--- scripts/blocks/environment/goo.js
+++ scripts/blocks/environment/goo.js
@@ -6,17 +6,21 @@ const electric = newEffect(30, e => {
 
   Draw.color(Color.valueOf("#F7F7F7");
 
+  /* Java version
   Angles.randLenVectors(e.id, 6, 2 + 19 * e.finpow(), (x, y) => {
     Fill.circle(e.x + x, e.y + y, e.fout() * 3 + 0.5);
     Fill.circle(e.x + x / 2, e.y + y / 2, e.fout() * 1);
   });
+  */
 
   Draw.color(Color.valueOf("#D4F2FB"), Color.valueOf("#A8E6F9"), Color.valueOf("#F7F7F7"), e.fin());
   Lines.stroke(1.5 * e.fout());
 
+  /* Java Version
   Angles.randLenVectors(e.id + 1, 8, 1 + 23 * e.finpow(), (x, y) => {
     Lines.lineAngle(e.x + x, e.y + y, Mathf.angle(x, y), 1 + e.fout() * 3);
   });
+  */
 });
 
 const goo = extendContent(Floor, "goo", {});
