fb26453abed95bd42bd90f23aec08f3bba96d30f
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-06-01 19:14:00 +0700

    what

--- scripts/blocks/walls/portaldoor.js
+++ scripts/blocks/walls/portaldoor.js
@@ -1,13 +1,13 @@
 const openD = newEffect(10, e => {
   Draw.color(Color.valueOf("ffde05"), Color.valueOf("ffdd00"), e.fin());
   Lines.stroke(e.fout() * 1.6);
-  Lines.square(e.x, e.y, this.size + e.fin() * 2);
+  Lines.square(e.x, e.y, this.tilesize + e.fin() * 2);
 });
 
 const closeD = newEffect(10, e => {
   Draw.color(Color.valueOf("0083cf"), Color.valueOf("0092e7"), e.fin());
   Lines.stroke(e.fout() * 1.6);
-  Lines.square(e.x, e.y, this.size + e.fout() * 2);
+  Lines.square(e.x, e.y, this.tilesize + e.fout() * 2);
 });
 
 const door = extendContent(Door, "portal-door", {
