388d62ffb998f910f4e4a5b1d3db042c9f49dd0f
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-06-01 18:19:42 +0700

    )

--- scripts/blocks/environment/goo.js
+++ scripts/blocks/environment/goo.js
@@ -7,7 +7,7 @@ const electric = newEffect(30, e => {
   */
              
   const hh = new Floatc2({get: function(x, y){
-    Draw.color(Color.valueOf("#F7F7F7");
+    Draw.color(Color.valueOf("#F7F7F7"));
     Fill.circle(e.x + x, e.y + y, e.fout() * 3 + 0.5);
     Fill.circle(e.x + x / 2, e.y + y / 2, e.fout() * 1);
   }});
@@ -37,7 +37,7 @@ const goo = extendContent(Floor, "goo", {
   draw(tile){
     Mathf.random.setSeed(tile.pos());
     
-    Draw.rect(this.region, tile.worldx(), tile.worldy();
+    Draw.rect(this.region, tile.worldx(), tile.worldy());
   }
 });
 
