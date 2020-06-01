fda374f9daaac9c93ded0bce21c55dcd792880e0
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-06-01 18:13:02 +0700

    Status Effect

---
+++ scripts/effects/electric.js
@@ -0,0 +1,36 @@
+const elecEffect = newEffect(30, e => {
+  /* Java version
+  Angles.randLenVectors(e.id, 6, 2 + 19 * e.finpow(), (x, y) => {
+    Fill.circle(e.x + x, e.y + y, e.fout() * 3 + 0.5);
+    Fill.circle(e.x + x / 2, e.y + y / 2, e.fout() * 1);
+  });
+  */
+             
+  const hh = new Floatc2({get: function(x, y){
+    Draw.color(Color.valueOf("#F7F7F7");
+    Fill.circle(e.x + x, e.y + y, e.fout() * 3 + 0.5);
+    Fill.circle(e.x + x / 2, e.y + y / 2, e.fout() * 1);
+  }});
+  Angles.randLenVectors(e.id, 6, 2 + 19, * e.finpow(), 30, hh);
+
+  /* Java Version
+  Angles.randLenVectors(e.id + 1, 8, 1 + 23 * e.finpow(), (x, y) => {
+    Lines.lineAngle(e.x + x, e.y + y, Mathf.angle(x, y), 1 + e.fout() * 3);
+  });
+  */
+
+  const hhh = new Floatc2({get: function(x, y){
+    Draw.color(Color.valueOf("#D4F2FB"), Color.valueOf("#A8E6F9"), Color.valueOf("#F7F7F7"), e.fin());
+    Lines.stroke(1.5 * e.fout());
+    Lines.lineAngle(e.x + x, e.y + y, Mathf.angle(x, y), 1 + e.fout() * 3);
+  }});
+  Angles.randLenVectors(e.id + 1, 8, 1 + 23 * e.finpow(), 30, hhh);
+});
+
+const electric = new StatusEffect("electric");
+  electric.damageMultiplier = 1;
+  electric.speedMultiplier = 0;
+  electric.armorMultiplier = 0;
+  electric.damage = 1.5;
+  electric.color = Color.valueOf("D4F2FB");
+  electric.effect = elecEffect;
