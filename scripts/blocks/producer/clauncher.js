ae1b33e6421284c51b6e81cd61ffcf2570480e76
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-06-05 11:46:28 +0700

    effect

--- scripts/blocks/producer/clauncher.js
+++ scripts/blocks/producer/clauncher.js
@@ -1,10 +1,44 @@
-/** const cl = extendContent (UnitFactory, "cube-factory", {
-    buildConfiguration(tile, table){
-        table.addImageButton(Icon.upOpen, Styles.clearTransi, run(() => {
-            tile.configure(0);
-        })).size(50).disabled(boolf(b => tile.entity !== null && !tile.entity.cons.valid()));
-    }
-    
-}); */
+const spawnEffect = newEffect(10, e => {
+  Draw.color(Color.valueOf("ffffff");
+  Lines.stroke(e.fout() * 5);
+  Lines.circle(e.x, e.y, e.fout() * 9);
+}
 
-//Already found a better solution
+const cl = extendContent (UnitFactory, "cube-factory", {
+  load(){
+    this.region = Core.atlas.find(this.name);
+  },
+  
+  generateIcons(){
+    return [
+      Core.atlas.find(this.name)
+    ]
+  },
+  
+  onUnitFactorySpawn(){
+            if(!(tile.entity instanceof UnitFactoryEntity) || !(tile.block() instanceof UnitFactory)) return;
+
+        entity = tile.ent();
+        factory = tile.block();
+
+        entity.buildTime = 0;
+        entity.spawned = spawns;
+
+        //Effects.shake(3f, 3f, entity);
+        Effects.effect(spawnEffect, tile.drawx(), tile.drawy());
+
+        if(!net.client()){
+            BaseUnit unit = factory.unitType.create(tile.getTeam());
+            unit.setSpawner(tile);
+            unit.set(tile.drawx() + Mathf.range(4), tile.drawy() + Mathf.range(4));
+            unit.add();
+            unit.velocity().y = factory.launchVelocity;
+            Events.fire(UnitCreateEvent(unit));
+        }
+  },
+    
+  draw(){
+    Draw.rect(this.region, tile.drawx(), tile.drawy());
+  }
+   
+});
