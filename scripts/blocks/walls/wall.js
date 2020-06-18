76f557ff731706d967107ec1297e7fd0ae7697e1
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-06-18 10:57:35 +0700

    Updoot power walls

--- scripts/blocks/walls/wall.js
+++ scripts/blocks/walls/wall.js
@@ -1,3 +1,5 @@
+const h = "portal";
+
 const bWall = extendContent(Wall, "black-wall", {
   handleBulletHit(entity, b){
     entity.damage(0);
@@ -21,3 +23,73 @@ const wWallLarge = extendContent(Wall, "white-wall-large", {
     entity.damage(0);
   }
 });
+
+const bWallP = extendContent(Wall, "black-wallp", {
+  load(){
+    this.region = Core.atlas.find(h + "-black-wall");
+  },
+  
+  generateIcons(){
+    return [
+      Core.atlas.find(h + "-black-wall"),
+      Core.atlas.find(h + "-pmark")
+    ]
+  },
+  
+  handleBulletHit(entity, b){
+    entity.damage(0);
+  }
+});
+
+const bWallLargeP = extendContent(Wall, "black-wall-largep", {
+  load(){
+    this.region = Core.atlas.find(h + "-black-wall-large");
+  },
+  
+  generateIcons(){
+    return [
+      Core.atlas.find(h + "-black-wall-large"),
+      Core.atlas.find(h + "-pmark-large")
+    ]
+  },
+  
+  handleBulletHit(entity, b){
+    entity.damage(0);
+  }
+});
+
+const wWallP = extendContent(Wall, "white-wallp", {
+  load(){
+    this.region = Core.atlas.find(h + "-white-wall");
+  },
+  
+  generateIcons(){
+    return [
+      Core.atlas.find(h + "-white-wall"),
+      Core.atlas.find(h + "-pmark")
+    ]
+  },
+  
+  handleBulletHit(entity, b){
+    entity.damage(0);
+  }
+});
+
+const wWallLargeP = extendContent(Wall, "white-wall-largep", {
+  load(){
+    this.region = Core.atlas.find(h + "-white-wall-large");
+  },
+  
+  generateIcons(){
+    return [
+      Core.atlas.find(h + "-white-wall-large"),
+      Core.atlas.find(h + "-pmark-large")
+    ]
+  },
+  
+  handleBulletHit(entity, b){
+    entity.damage(0);
+  }
+});
+
+
